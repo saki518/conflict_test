757ebd347c47ee37ace1d72d3758bad210d9c5f0
sasaki yuuto <y.sasaki.sys20@morijyobi.ac.jp>
2021-06-17 11:08:38 +0900

    a

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ app = Flask(__name__)
 @app.route("/")
 def top_page():
     return render_template("index.html")
+
 @app.route("/square_input")
 def square_input():
     return render_template("square_input.html")
@@ -14,6 +15,16 @@ def square_result():
     result=height*bottom
 
     return render_template("square_result.html",result=result)
-    
+
+@app.route("/circle_input")
+def circle_input():
+    return render_template("circle_input.html")
+
+@app.route("/circle_result")
+def circle_result():
+    radius = int(request.args.get("radius"))
+    result = 3.14 * radius ** 2
+    return render_template("circle_result.html", result=result)
+
 if __name__ == "__main__":
     app.run(debug=True)
